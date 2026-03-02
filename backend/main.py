62c89daa6d145a8823bf783428968cedd76a2c3d
MatiasValansi <valansimatias@gmail.com>
2026-03-02 16:58:06 -0300

    Se ajustó el Main para que se conecte con la base de datos cuando se ejecute el servidor.

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from backend.app.infrastructure.db.database import engine, Base
+from backend.app.infrastructure.db import models  # Importante para que SQLAlquemy vea los modelos
+
+# Esta línea crea las tablas en la base de datos si no existen
+# Es el equivalente al 'update' de Hibernate en Java
+Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Alethya SaaS API")
 
 @app.get("/")
 def read_root():
-    return {"status": "Alethya API is running", "version": "2.0-SaaS"}
+    return {
+        "status": "Alethya API is running", 
+        "version": "2.0-SaaS",
+        "database": "Connected and Tables Created"
+    }
