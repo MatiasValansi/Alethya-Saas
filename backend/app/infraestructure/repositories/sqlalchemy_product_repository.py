1b8054b5a5da5f74a1eb8f641346f2754f526a1f
MatiasValansi <valansimatias@gmail.com>
2026-03-02 16:47:55 -0300

    Se crea la conexión con la BD de la entidad Producto.

---
+++ backend/app/infraestructure/repositories/sqlalchemy_product_repository.py
@@ -0,0 +1,40 @@
+from typing import Optional
+from uuid import UUID
+from sqlalchemy.orm import Session
+from app.domain.entities.product import Product
+from app.domain.repositories.product_repository import ProductRepository
+from db.models import ProductModel
+
+class SqlAlchemyProductRepository(ProductRepository):
+    def __init__(self, db: Session):
+        self.db = db
+
+    def get_by_id(self, product_id: UUID, tenant_id: UUID) -> Optional[Product]:
+        # Buscamos en la DB usando el modelo de SQLAlchemy
+        model = self.db.query(ProductModel).filter(
+            ProductModel.id == product_id,
+            ProductModel.tenant_id == tenant_id
+        ).first()
+
+        if not model:
+            return None
+
+        # MAPEEO: Transformamos el modelo de DB a nuestra Entidad de Dominio
+        return Product(
+            id=model.id,
+            tenant_id=model.tenant_id,
+            name=model.name,
+            description=model.description,
+            price=model.price,
+            stock=model.stock,
+            code=model.code
+        )
+
+    def update(self, product: Product) -> None:
+        model = self.db.query(ProductModel).filter(
+            ProductModel.id == product.id
+        ).first()
+        
+        if model:
+            model.stock = product.stock # Actualizamos el stock tras la venta
+            self.db.commit()
