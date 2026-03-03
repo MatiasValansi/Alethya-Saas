2b86a7e7f1739db06d9d91bf05193705da08eeb2
MatiasValansi <valansimatias@gmail.com>
2026-03-03 14:54:45 -0300

    Se actualizaron los cambios implementados en la capa Use Case para que solo haya un llamado a la BD para realizar el UPDATE.

--- backend/app/use_cases/update_product.py
+++ backend/app/use_cases/update_product.py
@@ -7,18 +7,17 @@ class UpdateProductUseCase:
         self.product_repo = product_repo
 
     def execute(self, product_id: UUID, tenant_id: UUID, new_data: dict):
-        # 1. Orquestación: Buscar el producto
+        # 1. Traemos la entidad (Aquí SQLAlchemy ya tiene el objeto en "seguimiento")
         product = self.product_repo.get_by_id(product_id, tenant_id)
         if not product:
             raise ValueError("Producto no encontrado")
 
-        # 2. Lógica de Negocio: Usar la regla de la entidad
+        # 2. Validamos y actualizamos en memoria (Lógica de Dominio)
         product.update_data(
             name=new_data["name"],
             price=new_data["price"],
             stock=new_data["stock"]
         )
 
-        # 3. Persistencia: Guardar los cambios
-        self.product_repo.update(product)
-        return product
+        # 3. Persistimos (Una sola operación de guardado)
+        return self.product_repo.update(product)
