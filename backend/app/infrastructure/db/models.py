d4269f58492266d8bdf18e6482d5baa42caa59aa
MatiasValansi <valansimatias@gmail.com>
2026-03-09 17:36:30 -0300

    Se añadió la entidad Order Item a la tabla de la DB.

--- backend/app/infrastructure/db/models.py
+++ backend/app/infrastructure/db/models.py
@@ -41,3 +41,17 @@ class OrderModel(Base):
     
     # Relación para acceder a los items fácilmente
     items = relationship("OrderItemModel", back_populates="order", cascade="all, delete-orphan")
+    
+class OrderItemModel(Base):
+    __tablename__ = "order_items"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    order_id = Column(UUID(as_uuid=True), ForeignKey("orders.id"), nullable=False)
+    product_id = Column(UUID(as_uuid=True), ForeignKey("products.id"), nullable=False)
+    
+    quantity = Column(Integer, nullable=False)
+    price_at_sale = Column(Float, nullable=False) # Precio histórico de la venta
+    
+    # Relación inversa
+    order = relationship("OrderModel", back_populates="items")
+    product = relationship("ProductModel")
