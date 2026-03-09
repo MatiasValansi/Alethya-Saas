771b38956431d6d7f5a93fcc8a0924d8cdd39184
MatiasValansi <valansimatias@gmail.com>
2026-03-09 17:35:06 -0300

    Se añadió la entidad Order a la tabla de la BD.

--- backend/app/infrastructure/db/models.py
+++ backend/app/infrastructure/db/models.py
@@ -1,7 +1,9 @@
-from sqlalchemy import UUID, Column, String, Float, Integer, ForeignKey
+from sqlalchemy import UUID, Column, DateTime, String, Float, Integer, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID as PG_UUID
 import uuid
 from .database import Base
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 class ProductModel(Base):
     __tablename__ = "products"
@@ -24,3 +26,18 @@ class ClientModel(Base):
     email = Column(String)
     phone = Column(String)
     address = Column(String)
+    
+class OrderModel(Base):
+    __tablename__ = "orders"
+    
+    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    tenant_id = Column(UUID(as_uuid=True), nullable=False, index=True)
+    created_at = Column(DateTime, default=datetime.utcnow)
+    total = Column(Float, default=0.0)
+    status = Column(String, default="completed") # completed, pending, cancelled
+    
+    # Relación con el Cliente (FK)
+    client_id = Column(UUID(as_uuid=True), ForeignKey("clients.id"), nullable=True)
+    
+    # Relación para acceder a los items fácilmente
+    items = relationship("OrderItemModel", back_populates="order", cascade="all, delete-orphan")
