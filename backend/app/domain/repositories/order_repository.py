ac39c72318bc01771068d22cd40c1db959039a52
MatiasValansi <valansimatias@gmail.com>
2026-03-09 19:43:22 -0300

    Se añadió la definición del método Delete en la capa Repository de Order.

--- backend/app/domain/repositories/order_repository.py
+++ backend/app/domain/repositories/order_repository.py
@@ -18,3 +18,8 @@ class OrderRepository(ABC):
     def get_by_id(self, order_id: UUID, tenant_id: UUID):
         """Trae una orden por su ID y tenant_id."""
         pass
+    
+    @abstractmethod
+    def delete(self, order_id: UUID, tenant_id: UUID):
+        """Elimina una orden por su ID y tenant_id."""
+        pass
