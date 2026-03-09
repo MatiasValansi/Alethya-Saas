274a05e9de0f5fa2e7a0af1f6186cd163949677a
MatiasValansi <valansimatias@gmail.com>
2026-03-09 19:06:22 -0300

    Se añadieron los métodos Get All y Get By Id en el repositorio de Order.

--- backend/app/domain/repositories/order_repository.py
+++ backend/app/domain/repositories/order_repository.py
@@ -1,8 +1,20 @@
 from abc import ABC, abstractmethod
+from typing import List
+from uuid import UUID
 from app.domain.entities.order import Order
 
 class OrderRepository(ABC):
     @abstractmethod
     def save(self, order: Order) -> None:
         """Persiste la orden y sus items en la base de datos."""
+        pass
+    
+    @abstractmethod
+    def get_all(self, tenant_id: UUID) -> List:
+        """Trae todas las órdenes de un tenant específico."""
+        pass
+
+    @abstractmethod
+    def get_by_id(self, order_id: UUID, tenant_id: UUID):
+        """Trae una orden por su ID y tenant_id."""
         pass
