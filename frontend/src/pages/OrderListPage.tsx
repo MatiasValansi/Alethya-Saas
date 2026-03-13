d2cc98ddf193d9bbc9f076c3d1e4d5767c9a901a
MatiasValansi <valansimatias@gmail.com>
2026-03-13 00:00:23 -0300

    Se añadió el componente creado al Page de Order.

--- frontend/src/pages/OrderListPage.tsx
+++ frontend/src/pages/OrderListPage.tsx
@@ -5,17 +5,30 @@ import { OrderTable } from "@/components/orders/OrderTable";
 import { ShoppingCart, Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { OrderCreateView } from '@/components/orders/OrderCreateView';
+import type { Order } from '@/types/order';
+import { OrderDetailDialog } from '@/components/orders/OrderDetailDialog';
 
 export const OrderListPage = () => {
   const [view, setView] = useState<'list' | 'create'>('list');
   const { orders, addOrder, deleteOrder } = useOrders();
   const { loadProducts } = useProducts(); // <--- Para arreglar el stock
+  const [selectedOrder, setSelectedOrder] = useState<Order | null>(null);
+  const [isDetailOpen, setIsDetailOpen] = useState(false);
+
+  const handleViewOrder = (id: string) => {
+    const order = orders.find(o => o.id === id);
+    if (order) {
+      setSelectedOrder(order);
+      setIsDetailOpen(true);
+    }
+  };
 
   const handleCreateOrder = async (data: any) => {
     await addOrder(data);
     await loadProducts();
     setView('list'); 
   };
+  
 
   if (view === 'create') {
     return <OrderCreateView onCancel={() => setView('list')} onSubmit={handleCreateOrder} />;
@@ -27,7 +40,19 @@ export const OrderListPage = () => {
         <h2 className="text-2xl font-bold flex items-center gap-2"><ShoppingCart /> Gestión de Pedidos</h2>
         <Button onClick={() => setView('create')} className="bg-blue-600"><Plus className="mr-2 h-4 w-4" /> Armar nuevo pedido</Button>
       </div>
-      <OrderTable orders={orders} onView={() => {}} onDelete={deleteOrder} />
+      <OrderTable 
+        orders={orders} 
+        onView={handleViewOrder}
+        onDelete={deleteOrder} 
+      />
+
+      {/* AGREGAMOS EL COMPONENTE AL FINAL */}
+      <OrderDetailDialog 
+        order={selectedOrder}
+        isOpen={isDetailOpen}
+        onClose={() => setIsDetailOpen(false)}
+      />
+
     </div>
   );
 };
